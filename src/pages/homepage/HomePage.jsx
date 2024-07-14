47d6b5f1123bc6d0093c72c3aca2c26ffee4b4ee
BoloJueno <jonathan_bueno@dlsu.edu.ph>
2024-07-14 13:51:04 +0800

    added footer to homepage

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,9 +1,11 @@
 import Header from 'C:/xampp/htdocs/ani2home/src/components/Header.jsx'
+import Footer from 'C:/xampp/htdocs/ani2home/src/components/Footer.jsx'
 const HomePage = () => {
   return (
     <div className='w-full'>
       <Header />
       HOMEPAGE
+      <Footer />
     </div>
   );
 };
