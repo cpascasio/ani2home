8374e44f41fbc1c313c45e307865255e0382e80c
Zikamirichi <bittercandy30@gmail.com@website.com>
2024-07-15 00:22:17 +0800

    merge main to kei

--- src/pages/shopprofile/ShopProfile.jsx
+++ src/pages/shopprofile/ShopProfile.jsx
@@ -1,71 +1,72 @@
 import React from 'react';
+import Header from '../../components/Header.jsx'
+import Footer from '../../components/Footer.jsx'
 import './shopprofile.css';
 
 const ShopProfile = () => {
-  
-  return (
-    // insert navbar here
-    <div className="shopBanner">
-      <div className="shopLeftBox">
-        {/* include to shopPart1: Logo, Followers, Ratings, Products */}
-        <div className="shopPart1">
+    return (
+      <div className='w-full'>
+      <Header />
 
-          <div className="shopLogo">
-            <img src="../src/images/FarmShop1.jpg" alt="Shop Logo" />
-          </div>
-
-          <div className="shopStats">
-            <div className="shopFollowersIcon">
-              <img src="../src/images/FollowersIcon.png" alt="Followers" />
+        <div className="shopBanner">
+            <div className="shopLeftBox">
+                {/* include to shopPart1: Logo, Followers, Ratings, Products */}
+                <div className="shopPart1">
+                    <div className="shopLogo">
+                        <img src="../src/assets/FarmShop1.jpg" alt="Shop Logo" />
+                    </div>
+                    <div className="shopStats">
+                        <div className="shopStatItem shopStatsContainer">
+                            <div className="shopFollowersIcon">
+                                <img src="../src/assets/FollowersIcon.png" alt="Followers" />
+                            </div>
+                            <div className="shopFollowersCount">
+                                <strong>Followers:</strong> 1,203
+                            </div>
+                        </div>
+                        <div className="shopStatItem shopStatsContainer">
+                            <div className="shopRatingsIcon">
+                                <img src="../src/assets/RatingsIcon.png" alt="Ratings" />
+                            </div>
+                            <div className="shopRatingsCount">
+                              <strong>Rating:</strong> 4.4 (1,304)
+                            </div>
+                        </div>
+                        <div className="shopStatItem shopStatsContainer">
+                            <div className="shopProductsIcon">
+                                <img src="../src/assets/ProductsIcon.png" alt="Products" />
+                            </div>
+                            <div className="shopProductsCount">
+                              <strong>Products:</strong> 67
+                            </div>
+                        </div>
+                    </div> {/* end of shopStats */}
+                </div> {/* end of shopPart1 */}
+                <div className="shopPart2">
+                    <h1 className="shopTitle">
+                      Pogi Farms
+                    </h1>
+                    <div className="shopLocation"style={{ fontStyle: 'italic' }}>
+                      <i>Dasmarinas, Cavite</i>
+                    </div>
+                    <div className="shopDescription">
+                        Real eyes realize real lies. You miss the opportunities you don’t take.
+                    </div>
+                    <div className="shopButtons">
+                        follow and write a review button here
+                    </div>
+                </div> {/* end of shopPart2 */}
+            </div> {/* end of shopLeftBox */}
+            <div className="shopRightBox">
+                <div className="shopButtons">
+                    View Seller Button here
+                </div>
             </div>
-            <div className="shopFollowersCount">
-              Followers: insert numbers here
-            </div>
-
-            <div className="shopRatingsIcon">
-              <img src="../src/images/RatingsIcon.png" alt="Ratings" />
-            </div>
-            <div className="shopRatingsCount">
-                Rating: 4.4 (1,304)
-            </div>
-
-            <div className="shopProductsIcon">
-              <img src="../src/images/ProductsIcon.png" alt="Products" />
-            </div>
-            <div className="shopProductsCount">
-                Products: 67
-            </div>  
-          </div> {/* end of shopStats */}
-
-        </div> {/*end of shopPart1 */}
-
-        <div className="shopPart2">
-          <div className="shopTitle">
-            Pogi Farms
-          </div>
-          <div className="shopLocation">
-            Dasmarinas, Cavite
-          </div>
-          <div className="shopDescription">
-            Real eyes realize real lies. You miss the opportunities you don’t take.
-          </div>
-          <div className="shopButtons">
-            follow and write a review
-          </div>
-        </div> {/*end of shopPart2 */}
-
-      </div> {/*end of shopLeftBox */}
-
-
-      <div className="shopRightBox">
-        <div className="shopButtons">
-            View Seller Button here
-          </div>
-      </div>
-
-    </div> /*end of shopBanner */
-
-  );
+        </div>  {/* end of shopBanner */}
+      <Footer />
+      </div> /* end of classNAme w-full */
+        
+    );
 };
 
 export default ShopProfile;
