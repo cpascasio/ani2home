87e804a9d76931b89e1e95d5616c6e1a01dbe578
Zikamirichi <bittercandy30@gmail.com@website.com>
2024-07-15 17:06:14 +0800

    run test

--- frontend/src/components/routes.jsx
+++ frontend/src/components/routes.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import HomePage from "../pages/homepage/HomePage";
 import Login from "../pages/login/Login";
 import Products from "../pages/products/Products";
+import ShopProfile from "../pages/shopprofile/ShopProfile";
 
 
 const routes = [
     {/* CTRL + CLICK the elements to go to the file */ },
     { path: "/", element: <HomePage />, name: "Homepage" },
     { path: "/login", element: <Login />, name: "Login" },
-    { path: "/products", element: <Products />, name: "Products" }
+    { path: "/products", element: <Products />, name: "Products" },
+    { path: "/shopProfile", element: <ShopProfile />, name: "ShopProfile" }
     
 ];
 
