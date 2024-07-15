35ed91b064b979d322e2a7b70b7a941b72f60eda
BoloJueno <jonathan_bueno@dlsu.edu.ph>
2024-07-15 18:52:58 +0800

    Update userModels.js

--- backend/src/models/userModels.js
+++ backend/src/models/userModels.js
@@ -1,27 +1,24 @@
 const Joi = require('joi');
 
 const userSchema = Joi.object({
+    userId: Joi.string().guid({ version: 'uuidv4' }).required(),
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
+    userName: Joi.string().required(),
     email: Joi.string().email().required(),
     passwordHash: Joi.string().required(),
-    profilePictureUrl: Joi.string().uri(),
-    dateOfBirth: Joi.string().isoDate().required(),
+    dateOfBirth: Joi.date().default(() => new Date(), 'current date').iso().required(),
+    userProfilePic: Joi.string().default('[DEFAULT PROFILE PIC]'),
+    userCover: Joi.string().default('[DEFAULT COVER PIC]'),
     addresses: Joi.array().items(Joi.object({
       street: Joi.string().required(),
       city: Joi.string().required(),
       state: Joi.string().required(),
       postalCode: Joi.string().required()
     })).required(),
-    phoneNumbers: Joi.array().items(Joi.string().required()),
-    preferences: Joi.object({
-      newsletterSubscribed: Joi.boolean().required(),
-      notifications: Joi.object({
-        email: Joi.boolean().required(),
-        sms: Joi.boolean().required()
-      }).required()
-    }).required(),
-    followers: Joi.array().items(Joi.string().required()) // Array of other users' UIDs
+    phoneNumbers: Joi.array().items(Joi.string()).required(),
+    followers: Joi.array().items(Joi.string()).default([]).required(),
+    isVerified: Joi.boolean().default(false)
   });
 
   module.exports = userSchema;
