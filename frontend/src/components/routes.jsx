775c0328139fc570f351037f74a223c6bc9e670c
Zikamirichi <bittercandy30@gmail.com@website.com>
2024-07-18 16:23:01 +0800

    added routes for all My- for own profile

--- frontend/src/components/routes.jsx
+++ frontend/src/components/routes.jsx
@@ -3,6 +3,10 @@ import HomePage from "../pages/homepage/HomePage";
 import Login from "../pages/login/Login";
 import Products from "../pages/products/Products";
 import ShopProfile from "../pages/shopprofile/ShopProfile";
+import MyProfile from "../pages/myProfile/MyProfile";
+import MyOrders from "../pages/myOrders/MyOrders";
+import MyArticles from "../pages/myArticles/MyArticles";
+import MyShop from "../pages/myShop/MyShop";
 
 
 const routes = [
@@ -10,7 +14,11 @@ const routes = [
     { path: "/", element: <HomePage />, name: "Homepage" },
     { path: "/login", element: <Login />, name: "Login" },
     { path: "/products", element: <Products />, name: "Products" },
-    { path: "/shopProfile", element: <ShopProfile />, name: "ShopProfile" }
+    { path: "/shopProfile", element: <ShopProfile />, name: "ShopProfile" },
+    { path: "/myProfile", element: <MyProfile />, name: "MyProfile" },
+    { path: "/myOrders", element: <MyOrders />, name: "MyOrders" },
+    { path: "/myArticles", element: <MyArticles />, name: "MyArticles" },
+    { path: "/myShop", element: <MyShop />, name: "MyShop" }
     
 ];
 
