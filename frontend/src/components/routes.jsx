1476605b64d0bb97e020a39bf6af2d059fa6e411
cladzsan <claudio_santiago@dlsu.edu.ph>
2024-07-20 13:29:10 +0800

    Update routes.jsx
    Updated Routes

--- frontend/src/components/routes.jsx
+++ frontend/src/components/routes.jsx
@@ -19,7 +19,8 @@ const routes = [
     { path: "/myProfile", element: <MyProfile />, name: "MyProfile" },
     { path: "/myOrders", element: <MyOrders />, name: "MyOrders" },
     { path: "/myShop", element: <MyShop />, name: "MyShop" },
-    { path: "/cart", element: <Cart />, name: "Cart" }
+    { path: "/cart", element: <Cart />, name: "Cart" },
+    { path: "/checkout", element: <Checkout />, name: "Checkout" }
 ];
 
 export default routes;
