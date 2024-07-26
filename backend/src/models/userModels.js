88a681b5463e2ad7808eecd61da01cf1dd124d42
cpascasio <ceejay1110@gmail.com>
2024-07-26 17:02:09 +0800

    added: google login fixed

--- backend/src/models/userModels.js
+++ backend/src/models/userModels.js
@@ -2,15 +2,14 @@ const Joi = require('joi');
 
   const userSchema = Joi.object({
     userId: Joi.string().required(),
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
+    name: Joi.string().default(""),
     userName: Joi.string().required(),
     email: Joi.string().email().required(),
-    dateOfBirth: Joi.date().iso().required(),
+    dateOfBirth: Joi.date().iso().default(""),
     userProfilePic: Joi.string().default('https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png'),
     userCover: Joi.string().default('https://walker-web.imgix.net/cms/Gradient_builder_2.jpg?auto=format,compress&w=1920&h=1200&fit=crop&dpr=1.5'),
-    address: Joi.string().required(),
-    phoneNumber: Joi.string().required(),
+    address: Joi.string().default(""),
+    phoneNumber: Joi.string().default(""),
     followers: Joi.array().items(Joi.string()).default([]),
     isVerified: Joi.boolean().default(false)
 });
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -10,23 +10,21 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-    useEffect(() => {
-        auth.onAuthStateChanged((user) => {
-            if (user) {
-                setAuthenticated(true);
-                window.localStorage.setItem('authenticated', 'true');
-                user.getIdTokenResult().then((token) => {
-                    console.log(token);
-                    setToken(token.token);
-                });
-            }
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAuthenticated(true);
+        window.localStorage.setItem('authenticated', 'true');
+        user.getIdTokenResult().then((tokenResult) => {
+          console.log(tokenResult);
+          setToken(tokenResult.token);
         });
-    }
-    , []);
+      }
+    });
+  }, []);
 
-
-    useEffect(() => {
-      console.log("AUTHENTICATED? " + window.localStorage.getItem('authenticated'));
+  useEffect(() => {
+    console.log("AUTHENTICATED? " + window.localStorage.getItem('authenticated'));
   }, []);
 
   const handleLogin = async () => {
@@ -50,56 +48,96 @@ const Login = () => {
     setAuthenticated(false);
     window.localStorage.removeItem('authenticated');
     auth.signOut();
-};
+  };
 
-const handleEmailLogin = async (email, password) => {
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    // User is signed in
-    // You can access the user object via userCredential.user
-    console.log(userCredential.user);
-    // Update authenticated state or perform other actions
-    setAuthenticated(true);
-    window.localStorage.setItem('authenticated', 'true');
-    // Optionally, get the token as you did in the useEffect
-    const tokenResult = await userCredential.user.getIdTokenResult();
-    setToken(tokenResult.token);
-  } catch (error) {
-    // Handle Errors here.
-    console.error(error.message);
-    // Optionally, update UI to reflect the error
-  }
-};
+  const handleEmailLogin = async (email, password) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      // User is signed in
+      // You can access the user object via userCredential.user
+      console.log(userCredential.user);
+      // Update authenticated state or perform other actions
+      setAuthenticated(true);
+      window.localStorage.setItem('authenticated', 'true');
+      // Optionally, get the token as you did in the useEffect
+      const tokenResult = await userCredential.user.getIdTokenResult();
+      setToken(tokenResult.token);
+    } catch (error) {
+      // Handle Errors here.
+      console.error(error.message);
+      // Optionally, update UI to reflect the error
+    }
+  };
 
-const handleEmailSignUp = async (email, password) => {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    // User account created & signed in
-    console.log(userCredential.user);
-    // Update authenticated state or perform other actions
-    setAuthenticated(true);
-    window.localStorage.setItem('authenticated', 'true');
-    // Optionally, get the token as you did in the useEffect
-    const tokenResult = await userCredential.user.getIdTokenResult();
-    setToken(tokenResult.token);
-  } catch (error) {
-    // Handle Errors here.
-    console.error(error.message);
-    // Optionally, update UI to reflect the error
-  }
-};
+  const handleEmailSignUp = async (email, password) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      // User account created & signed in
+      console.log(userCredential.user);
+      // Update authenticated state or perform other actions
+      setAuthenticated(true);
+      window.localStorage.setItem('authenticated', 'true');
+      // Optionally, get the token as you did in the useEffect
+      const tokenResult = await userCredential.user.getIdTokenResult();
+      setToken(tokenResult.token);
+    } catch (error) {
+      // Handle Errors here.
+      console.error(error.message);
+      // Optionally, update UI to reflect the error
+    }
+  };
 
   const handleGoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
+      console.log("🚀 ~ handleGoogleLogin ~ result:", result)
       const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
+      console.log("🚀 ~ handleGoogleLogin ~ credential:", credential)
+      //const token = credential.accessToken;
       const user = result.user;
+      
       console.log(user);
+  
       // Here, you can use the token or user information for your application's login logic
       if (user) {
         setAuthenticated(true); // Set auth to true if Google login is successful
         window.localStorage.setItem('authenticated', 'true');
+        // set token
+        
+  
+        // Retrieve the Firebase token instead of the Google token for backend API calls
+        const tokenResult = await user.getIdTokenResult();
+        console.log("🚀 ~ handleGoogleLogin ~ tokenResult:", tokenResult)
+        //setToken(tokenResult);
+
+        const userId = result?._tokenResponse.localId;
+        const email = user.providerData[0]?.email;
+        const userName = user.providerData[0]?.displayName?.replace(/\s+/g, '');
+        const phoneNumber = user.providerData[0]?.phoneNumber;
+        const userProfilePic = user.providerData[0]?.photoURL;
+        const name = result?._tokenResponse.firstName + " " + result?._tokenResponse.lastName;
+  
+        console.log(userId, email, userName, phoneNumber, userProfilePic, name);
+  
+        // Only create the user in the backend if they are a new user
+        // Only create the user in the backend if they are a new user
+      if (result?._tokenResponse.isNewUser) {
+        const payload = {};
+
+        if (userId) payload.userId = userId;
+        if (email) payload.email = email;
+        if (userName) payload.userName = userName;
+        if (phoneNumber) payload.phoneNumber = phoneNumber;
+        if (userProfilePic) payload.userProfilePic = userProfilePic;
+        if (name.trim()) payload.name = name;
+
+        await axios.post("http://localhost:5000/api/users/create-user", payload, {
+          headers: {
+            'Authorization': `Bearer ${tokenResult.token}`,
+            'Content-Type': 'application/json'
+          }
+        });
+      }
       }
     } catch (error) {
       console.error(error);
@@ -108,7 +146,6 @@ const handleEmailSignUp = async (email, password) => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center">
-        
       <div>
         <input
           type="text"
@@ -128,21 +165,16 @@ const handleEmailSignUp = async (email, password) => {
       <div>
         <button onClick={handleLogin}>Login</button>
 
-{
-    authenticated ? (
-        <div>
+        {authenticated ? (
+          <div>
             <h1>Authenticated</h1>
             <button onClick={handleLogout}>Logout</button>
-            <Sample token={token} />
-        </div>
-    ) : (
-        <div>
-          <button onClick={handleGoogleLogin}>Login with Google</button>
-        </div>
-    )
-}
-
-        
+          </div>
+        ) : (
+          <div>
+            <button onClick={handleGoogleLogin}>Login with Google</button>
+          </div>
+        )}
       </div>
     </div>
   );
