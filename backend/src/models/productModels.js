66eef1de2382a18a69962fa5108c31db129ecebf
cpascasio <ceejay1110@gmail.com>
2024-07-28 06:28:33 +0800

    updated products

--- backend/src/models/productModels.js
+++ backend/src/models/productModels.js
@@ -1,18 +1,40 @@
 const Joi = require('joi');
 
+const now = new Date().toISOString();
+
 const productSchema = Joi.object({
     // productId: Joi.string().guid({ version: 'uuidv4' }).required(),
     storeId: Joi.string().default(""),
     productName: Joi.string().required(),
-    productDescription: Joi.string().required(),    
-    productDate: Joi.date().iso().default(""),
-    rating: Joi.number().min(0).max(5).precision(1).required(),
+    productDescription: Joi.string().required(),
+    dateAdded: Joi.date().iso().default(now),
+    rating: Joi.number().min(0).max(5).precision(1).default(0),
+    isKilo: Joi.boolean().default(false),
     totalSales: Joi.number().required().default(0),
-    stock: Joi.number().required().default(0),
-    price: Joi.number().precision(2).required().required(),
+    stock: Joi.number().required(),
+    price: Joi.number().precision(2).required(),
     pictures: Joi.array().items(Joi.string()).max(5).required(),
-    variation: Joi.array().items(Joi.string()).default([]),
-    category: Joi.string().valid('Vegetable', 'Meat', 'Fruit').required()
+    type: Joi.string().required(),
+    category: Joi.string().valid('Vegetable', 'Fruit').required()
 });
 
 module.exports = productSchema;
+
+// modal specifications
+
+// Name
+// Description
+// category (vegetable, fruit)
+// create a dropdown that follows these conditions:
+// if(vegetable) dropdown should have a list of vegetables
+// if(fruit) dropdown should have a list of fruits
+
+// if dropdown has selected empty, create a custom other input field.
+
+// kilo or pieces
+// price per kilo or per piece
+// Stock
+
+
+// pictures
+
--- frontend/src/pages/seller/Seller.jsx
+++ frontend/src/pages/seller/Seller.jsx
@@ -1,11 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useUser } from "../../context/UserContext";
 import useFetch from "../../../hooks/useFetch";
 import InventoryTable from "../../components/seller/InventoryTable";
+import Menu from "../../components/seller/Menu";
 
 
 // this component contains the Seller Dashboard
 const Seller = () => {
+
+    const [selectedMenu, setSelectedMenu] = useState('dashboard');
+
+    const renderContent = () => {
+        switch (selectedMenu) {
+          case 'dashboard':
+            return <div>Dashboard Content</div>;
+          case 'inventory':
+            return <InventoryTable />;
+          case 'summary':
+            return <div>Summary Content</div>;
+          case 'purchase':
+            return <div>Purchase Content</div>;
+          case 'suppliers':
+            return <div>Suppliers Content</div>;
+          case 'sales':
+            return <div>Sales Content</div>;
+          case 'invoice':
+            return <div>Invoice Content</div>;
+          case 'bill':
+            return <div>Bill Content</div>;
+          case 'customers':
+            return <div>Customers Content</div>;
+          default:
+            return <div>Dashboard Content</div>;
+        }
+      };
 
     const { user } = useUser();
 
@@ -26,13 +54,14 @@ const Seller = () => {
         <div>
             {
                 user ? (
-                    <div>
-                        <h1>Welcome {user.username}</h1>
-                        <p>Here you can manage your products and orders</p>
 
-                        <InventoryTable />
-
-
+                    <div className="flex">
+                        <div className="menu-container">
+                            <Menu onSelectMenu={setSelectedMenu} />
+                        </div>
+                        <div className="content-container">
+                            {renderContent()}
+                        </div>
                     </div>
                 ) : (
                     <div>
