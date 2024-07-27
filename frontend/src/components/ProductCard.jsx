25c6583b35a022286f6e23c0e10aeaa04d6ec069
minettearmada <114899727+minettearmada@users.noreply.github.com>
2024-07-28 04:05:11 +0800

    Successfully fetches the products data

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import { useState } from 'react';
-const [products, setProducts] = useState([]);
+import { format } from 'date-fns';
 
-const { data: productsFetch } = useFetch("/api/products");
 
-useEffect(() => {
-    if (productsFetch) {
-        setProducts(productsFetch);
-    }
-}, [productsFetch]);
-
-const ProductCard = () => {
+const ProductCard = (product) => {  
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure className="px-10 pt-10">
         <img
-          src={productsFetch.pictures[0]}
-          alt="Shoes"
+          src={product.pictures[0]}
+          alt={product.productName}
           className="rounded-xl"
         />
       </figure>
       <div className="card-body items-center text-center">
-        <h2 className="card-title">{productsFetch.productName}</h2>
-        <p>{productsFetch.productDescription}</p>
+        <h2 className="card-title">{product.productName}</h2>
+        <p>{product.productDescription}</p>
+        <p>Php {product.price}</p>
+        <p>Rating: {product.rating}</p>
+        {/* <p>Date: {product.productDate}</p> */}
         <div className="card-actions">
           <button className="btn btn-primary">Add To Cart</button>
         </div>
--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -1,4 +1,5 @@
 // import ProductCard component
+import { useState, useEffect } from 'react';
 import ProductCard from '../../components/ProductCard';
 import useFetch from '../../../hooks/useFetch'
 
@@ -6,10 +7,34 @@ import useFetch from '../../../hooks/useFetch'
 
 const Products = () => { // function name should match the file name
 
+  const [products, setProducts] = useState([]);
+
+  const { data: productsFetch } = useFetch("/api/products");
+  
+
+  useEffect(() => {
+    if (productsFetch) {
+      setProducts(productsFetch);
+    }
+  }
+  , [productsFetch]);
+
+
+  useEffect(() => {
+    console.log(products)
+  }
+  , [products]);
+
+
     return (
       <div className="flex w-full border-2">
         <button className="btn btn-outline btn-info">Info</button>
-        <ProductCard/>
+        {
+          products?.map((product) => (
+            <ProductCard key={product._id} {...product} />
+          ))
+        }
+
       </div>
     );
   };
