243c91df362c69ca963a091bcfbad3677bda589c
cpascasio <ceejay1110@gmail.com>
2024-08-01 05:18:47 +0800

    added: lalamove config

--- frontend/layout/Layout.jsx
+++ frontend/layout/Layout.jsx
@@ -4,6 +4,8 @@ import { UserProvider } from "../src/context/UserContext";
 import { CartProvider } from "../src/context/CartContext";
 import PageRouter from "../src/components/PageRouter";
 import { BrowserRouter as Router } from "react-router-dom";
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
 
 
 const Layout = () => {
@@ -11,7 +13,9 @@ const Layout = () => {
     <UserProvider>
     <CartProvider>
     <Router>
+      <Header />
         <PageRouter />
+        <Footer />
     </Router>
     </CartProvider>
     </UserProvider>
