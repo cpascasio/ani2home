db75371a3cc03798b67f0b16862b651842727ad1
BoloJueno <jonathan_bueno@dlsu.edu.ph>
2024-08-02 23:40:37 +0800

    Update userModels.js

--- backend/src/models/userModels.js
+++ backend/src/models/userModels.js
@@ -2,26 +2,26 @@ const Joi = require('joi');
 
   const userSchema = Joi.object({
     userId: Joi.string().optional(),
-    name: Joi.string().default(""),
+    name: Joi.string().optional(),
     userName: Joi.string().optional(),
     email: Joi.string().email().optional(),
-    dateOfBirth: Joi.date().iso().default(""),
+    dateOfBirth: Joi.date().iso().optional(),
     userProfilePic: Joi.string().default('../src/assets/MyProfile pic.png'),
-    userCover: Joi.string().default('https://walker-web.imgix.net/cms/Gradient_builder_2.jpg?auto=format,compress&w=1920&h=1200&fit=crop&dpr=1.5'),
+    userCover: Joi.string().optional(),
     address: Joi.object({
-      fulladdress: Joi.string().default(""),
-      street: Joi.string().default(""),
-      city: Joi.string().default(""),
-      province: Joi.string().default(""),
-      region: Joi.string().default(""),
-      country: Joi.string().default(""),
+      fulladdress: Joi.string().optional(),
+      street: Joi.string().optional(),
+      city: Joi.string().optional(),
+      province: Joi.string().optional(),
+      region: Joi.string().optional(),
+      country: Joi.string().optional(),
       lng: Joi.number().default(0),
       lat: Joi.number().default(0),
     }).default(),
-    phoneNumber: Joi.string().default(""),
+    phoneNumber: Joi.string().optional(),
     followers: Joi.array().items(Joi.string()).default([]),
     isStore : Joi.boolean().default(false),
-    bio: Joi.string().default(""),
+    bio: Joi.string().optional(),
     isVerified: Joi.boolean().default(false)
 });
 
