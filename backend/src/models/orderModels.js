729aef1b2320870b48d0a33dce99f6032f88a99d
sha9bl8 <shannyne.blythe@gmail.com>
2025-03-30 23:43:26 +0800

    fixed ordermodels.js

--- backend/src/models/orderModels.js
+++ backend/src/models/orderModels.js
@@ -1,36 +1,5 @@
-// const Joi = require('joi');
-
-// const orderSchema = Joi.object({
-//     orderId: Joi.string().required(),
-//     orderDate: Joi.date().default(() => new Date(), 'current date').iso().required(),
-//     orderStatus: Joi.string().valid('In Progress', 'Shipped', 'Disputed', 'Resolved', 'Completed').required().default('In Progress'),
-//     orderPayment: Joi.string().valid('COD', 'GCash', 'Bank').required()
-// });
-
-// module.exports = orderSchema;
 const Joi = require("joi");
 
-/*
-
-const orderSchema = Joi.object({
-    userId: Joi.string().optional(),
-    items: Joi.array().items(Joi.object({
-      productId: Joi.string().optional(),
-      quantity: Joi.number().optional()
-    })).required(),
-    totalPrice: Joi.number().required(),
-    deliveryAddress: Joi.object({
-      fullName: Joi.string().required(),
-      countryCode: Joi.string().required(),
-      phoneNumber: Joi.string().required(),
-      address: Joi.string().default('')
-    }).required(),
-    note: Joi.string().optional(),
-    paymentOption: Joi.string().required()
-  });
-
-
-  */
 
 const orderSchema = Joi.object({
   userId: Joi.string().optional(),
